9c5883b5f8596bdeed62a4bc770ed2dfcb725615
LuizMeraki <luizollvrsantos@gmail.com>
2023-05-25 09:30:34 -0300

    feat: created ReducerType

--- src/@types/form.ts
+++ src/@types/form.ts
@@ -1,5 +1,9 @@
+import { Dispatch } from 'react'
+
 export type ActionType = {
   actionType: string
   propKey: string;
   payload: string | null
 }
+
+export type ReducerType = Dispatch<ActionType>
