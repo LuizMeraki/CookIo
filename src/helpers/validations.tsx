6fd611aeb9c6ea809fcfc276fa0932d693d4025f
LuizMeraki <luizollvrsantos@gmail.com>
2023-05-28 12:02:35 -0300

    feat: now validations handle state value

--- src/helpers/validations.tsx
+++ src/helpers/validations.tsx
@@ -1,94 +1,88 @@
-import { ReducerType } from '~/@types/form';
+import { ChangeEvent } from 'react';
+import { DispatchType, ActionType } from '~/@types/form';
 
-const defaultPropKey = 'error';
+function verifyLength(value: string, min: number, max: number) {
+  const length = value.trim().length;
+
+  if (length < min) return `informe no mínimo ${min} caracteres`
+  if (length > max) return `informe no máximo ${max} caracteres`
+
+  return null;
+}
 
 export function validateLength(
-  dispatch: ReducerType,
-  actionType: string,
-  value: string,
+  e: ChangeEvent<HTMLInputElement>,
+  dispatch: DispatchType,
+  type: ActionType,
   min: number,
   max: number
 ) {
-  const length = value.trim().length;
+  const value = e.target.value;
+  const validationValue = verifyLength(value, min, max);
 
-  if (length < min) {
-    dispatch({
-      actionType,
-      propKey: defaultPropKey,
-      payload: `informe no mínimo ${min} caracteres`,
-    });
-
-    return true;
-  } else if (length > max) {
-    dispatch({
-      actionType,
-      propKey: defaultPropKey,
-      payload: `informe no máximo ${max} caracteres`,
-    });
-
-    return true;
-  } else {
-    dispatch({ actionType, propKey: defaultPropKey, payload: null });
-
-    return false;
-  }
+  dispatch({
+    type,
+    payload: { value, error: validationValue },
+  });
 }
 
 export function validateName(
-  dispatch: ReducerType,
-  actionType: string,
-  value: string,
+  e: ChangeEvent<HTMLInputElement>,
+  dispatch: DispatchType,
+  type: ActionType,
   min: number,
   max: number
 ) {
+  const value = e.target.value;
+  const validationValue = verifyLength(value, min, max)
   const hasNonAlphabeticCharacters = /[^A-Za-z\s]/.test(value);
-  const hasLengthError = validateLength(dispatch, actionType, value, min, max);
 
   if (hasNonAlphabeticCharacters) {
     dispatch({
-      actionType,
-      propKey: defaultPropKey,
-      payload: 'informe apenas caracteres alfabéticos',
+      type,
+      payload: { value, error: 'informe apenas caracteres alfabéticos' },
     });
-  } else if (!hasLengthError) {
-    dispatch({ actionType, propKey: defaultPropKey, payload: null });
+  } else {
+    dispatch({ type, payload: { value, error: validationValue }});
   }
 }
 
-export function validateEmail(dispatch: ReducerType, actionType: string, value: string) {
-  const trimmedValue = value.trim();
+export function validateEmail(
+  e: ChangeEvent<HTMLInputElement>,
+  dispatch: DispatchType,
+  type: ActionType
+) {
+  const value = e.target.value;
   const regularExpression = /^[a-z0-9._-]+@(gmail|hotmail|outlook|yahoo)\.com$/;
-  const isEmailValid = regularExpression.test(trimmedValue);
+  const isEmailValid = regularExpression.test(value);
 
   if (!isEmailValid) {
     dispatch({
-      actionType,
-      propKey: defaultPropKey,
-      payload: 'informe um e-mail válido',
+      type,
+      payload: { value, error: 'informe um e-mail válido' },
     });
   } else {
-    dispatch({ actionType, propKey: defaultPropKey, payload: null });
+    dispatch({ type, payload: { value, error: null } });
   }
 }
 
 export function validatePassword(
-  dispatch: ReducerType,
-  actionType: string,
-  value: string,
+  e: ChangeEvent<HTMLInputElement>,
+  dispatch: DispatchType,
+  type: ActionType,
   min: number,
   max: number
 ) {
-  const trimmedValue = value.trim();
-  const isPasswordValid = /^[a-zA-Z0-9._!@#$%&]+$/g.test(trimmedValue);
-  const hasLengthError = validateLength(dispatch, actionType, value, min, max);
-
+  const value = e.target.value;
+  const isPasswordValid = /^[a-zA-Z0-9._!@#$%&]+$/g.test(value);
+  const validationValue = verifyLength(value, min, max);
+  
   if (!isPasswordValid) {
     dispatch({
-      actionType,
-      propKey: defaultPropKey,
-      payload: 'informe apenas caracteres de a-z, 0-9 ou ._!@#$%&',
+      type,
+      payload: { value, error: 'informe apenas caracteres de a-z, 0-9 ou ._!@#$%&' },
     });
-  } else if (!hasLengthError && isPasswordValid){
-    dispatch({ actionType, propKey: defaultPropKey, payload: null });
+  } else {
+    dispatch({ type, payload: { value, error: validationValue } });
   }
 }
