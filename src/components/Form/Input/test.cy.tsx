e0436082a4cd2b7012a3a5083e2e4956d3273266
LuizMeraki <luizollvrsantos@gmail.com>
2023-05-30 15:57:01 -0300

    feat: add new tests

--- src/components/Form/Input/test.cy.tsx
+++ src/components/Form/Input/test.cy.tsx
@@ -6,7 +6,10 @@ import { reducerMock, initialValueMock } from './mock';
 
 import Input from '.';
 
-function InputInstance() {
+const minLength = 2;
+const maxLength = 2;
+
+function InputMock() {
   const [state, dispatch] = useReducer(reducerMock, initialValueMock);
 
   return (
@@ -16,13 +19,42 @@ function InputInstance() {
       placeholder="seu nome"
       error={state.username!.error}
       state={state.username!.value}
-      onChange={(e) => validateName(e, dispatch, 'USERNAME', 2, 50)}
+      onChange={(e) => validateName(e, dispatch, 'USERNAME', minLength, maxLength)}
     />
   );
 }
 
 describe('Input', () => {
-  it('Should mount', () => {
-    cy.mount(<InputInstance />);
+  beforeEach(() => {
+    cy.mount(<InputMock />);
+  });
+
+  it('Should have input focus on label click', () => {
+    cy.get('label').click();
+    cy.get('input').focused();
+  });
+
+  it('Should not have typing error', () => {
+    cy.get('input').type('Jhon Doe');
+    cy.get('label').should('not.have.text', 'informe apenas caracteres alfabéticos');
+  });
+
+  it('Should have typing error', () => {
+    cy.get('input').type('#');
+    cy.get('span').should('have.text', 'informe apenas caracteres alfabéticos');
+  });
+
+  it('Should have error class', () => {
+    cy.get('input').type('#').should('have.class', 'styles_error__7YWZd');
+  });
+
+  it('Should have min length error', () => {
+    cy.get('input').type('J');
+    cy.get('span').should('have.text', `informe no mínimo ${minLength} caracteres`);
+  });
+
+  it('Should have max length error', () => {
+    cy.get('input').type('JhonDoe'.repeat(5));
+    cy.get('span').should('have.text', `informe no máximo ${maxLength} caracteres`);
   });
 });
