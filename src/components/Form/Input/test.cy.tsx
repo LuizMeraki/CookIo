13e459e875d0ad3f887402d47376ad3db75e6b26
LuizMeraki <luizollvrsantos@gmail.com>
2023-05-30 13:04:56 -0300

    refactor: using reducer from input mock.ts

--- src/components/Form/Input/test.cy.tsx
+++ src/components/Form/Input/test.cy.tsx
@@ -1,37 +1,21 @@
 import { useReducer } from 'react';
 
-import { ReducerActionType } from '~/@types/form';
-
 import { validateName } from '~/helpers/validations';
+
+import { reducerMock, initialValueMock } from './mock';
 
 import Input from '.';
 
-type InitialValueType = { username: { value: string, error: string | null } }
-
-const initialValue: InitialValueType = { username: { value: '', error: null } };
-
-function reducer(state: InitialValueType, action: ReducerActionType) {
-  const value = action.payload.value;
-  const error = action.payload.error;
-
-  switch (action.type) {
-    case 'USERNAME':
-      return { ...state, username: { value, error } };
-    default:
-      return state;
-  }
-}
-
 function InputInstance() {
-  const [state, dispatch] = useReducer(reducer, initialValue);
+  const [state, dispatch] = useReducer(reducerMock, initialValueMock);
 
   return (
     <Input
       label="Nome"
       type="text"
       placeholder="seu nome"
-      error={state.username.error}
-      state={state.username.value}
+      error={state.username!.error}
+      state={state.username!.value}
       onChange={(e) => validateName(e, dispatch, 'USERNAME', 2, 50)}
     />
   );
