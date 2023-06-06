b9540de80013406b7511665b9208f074088669bb
Luiz Meraki <luizollvrsantos@gmail.com>
2023-06-06 17:14:34 -0300

    feat: created the whole component structure

---
+++ src/components/Form/Button/index.tsx
@@ -0,0 +1,35 @@
+import { lazy, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
+
+const Link = lazy(() => import('next/link'));
+
+import styles from './styles.module.scss';
+
+type ElementsType = ButtonHTMLAttributes<HTMLButtonElement> &
+  AnchorHTMLAttributes<HTMLAnchorElement>;
+
+interface ButtonProps extends ElementsType {
+  children: React.ReactNode;
+  hasAnchor?: boolean;
+  anchor?: string;
+}
+
+export default function Button({
+  children,
+  hasAnchor = false,
+  anchor = '',
+  ...rest
+}: ButtonProps) {
+  return (
+    <>
+      {hasAnchor ? (
+        <Link className={styles.element} href={anchor}>
+          {children}
+        </Link>
+      ) : (
+        <button className={styles.element} {...rest}>
+          {children}
+        </button>
+      )}
+    </>
+  );
+}
