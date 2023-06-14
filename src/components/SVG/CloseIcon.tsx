7b1792019980e7c31dd145dd7ceb51d8c86f179a
Luiz Meraki <luizollvrsantos@gmail.com>
2023-06-14 16:17:40 -0300

    feat: created the svg component structure

---
+++ src/components/SVG/CloseIcon.tsx
@@ -0,0 +1,17 @@
+import { SvgProp } from '~/@types/Svg';
+
+export function CloseIcon({ className }: SvgProp) {
+  return (
+    <svg
+      className={className}
+      width="22"
+      height="21"
+      viewBox="0 0 22 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1.33911 1.80132L20.6609 19.1987" stroke="#ADA5A5" strokeWidth="3" />
+      <path d="M1.33911 19.1987L20.6609 1.8013" stroke="#ADA5A5" strokeWidth="3" />
+    </svg>
+  );
+}
