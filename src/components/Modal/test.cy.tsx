06b2098080e8b59af9e0095a36a003db53462013
Luiz Meraki <luizollvrsantos@gmail.com>
2023-06-15 14:47:05 -0300

    feat: created tests for modal

---
+++ src/components/Modal/test.cy.tsx
@@ -0,0 +1,47 @@
+import Modal from '.';
+import { RequestErrorTemplate } from './templates/RequestError';
+import { handleModalRender } from '~/helpers/handleModal';
+
+describe('Modal', () => {
+  const primaryModalId = 'modal-primary';
+  const secondaryModalId = 'modal-secondary';
+
+  beforeEach(() => {
+    cy.mount(
+      <>
+        <Modal id={primaryModalId}>
+          <RequestErrorTemplate />
+        </Modal>
+
+        <Modal id={secondaryModalId}>
+          <RequestErrorTemplate />
+        </Modal>
+      </>
+    );
+  });
+
+  it('Should not render Modal', () => {
+    cy.get('dialog').should('not.have.attr', 'open');
+  });
+
+  it('Should render primary Modal', () => {
+    handleModalRender(primaryModalId);
+
+    cy.get(`#${secondaryModalId}`).should('not.have.attr', 'open');
+    cy.get(`#${primaryModalId}`).should('have.attr', 'open');
+  });
+
+  it('Should render secondary Modal', () => {
+    handleModalRender(secondaryModalId);
+
+    cy.get(`#${primaryModalId}`).should('not.have.attr', 'open');
+    cy.get(`#${secondaryModalId}`).should('have.attr', 'open');
+  });
+
+  it('Should close Modal', () => {
+    handleModalRender(primaryModalId);
+
+    cy.get('.styles_closeIcon__HpqQe').first().click();
+    cy.get('dialog').should('not.have.attr', 'open');
+  });
+});
