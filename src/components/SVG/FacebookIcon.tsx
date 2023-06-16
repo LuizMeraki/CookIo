f6e15cb38f72737ed12e7a1fc29085aad34b5ee0
Luiz Meraki <luizollvrsantos@gmail.com>
2023-06-16 16:18:16 -0300

    feat: created facebook icon component

---
+++ src/components/SVG/FacebookIcon.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+
+import { SvgProp } from '~/@types/Svg';
+
+export function FacebookIcon({ className }: SvgProp) {
+  return (
+    <svg
+      className={className}
+      aria-label="Logo do Facebook"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0Z"
+        fill="#039BE5"
+      />
+      <path
+        d="M11.3535 12.6505H13.9414L14.3477 10.0216H11.3529V8.58475C11.3529 7.49265 11.7098 6.52423 12.7314 6.52423H14.3729V4.23001C14.0845 4.19107 13.4745 4.1058 12.3219 4.1058C9.91505 4.1058 8.504 5.37686 8.504 8.27265V10.0216H6.02979V12.6505H8.504V19.8763C8.994 19.95 9.49031 20 9.99978 20C10.4603 20 10.9098 19.9579 11.3535 19.8979V12.6505Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
