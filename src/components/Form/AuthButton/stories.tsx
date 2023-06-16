984fddff880a318549fb1dec3f02d57e67d48bdd
Luiz Meraki <luizollvrsantos@gmail.com>
2023-06-16 16:21:25 -0300

    feat: created the component stories

---
+++ src/components/Form/AuthButton/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import AuthButton from '.';
+
+type AuthButtonType = typeof AuthButton;
+type StoryType = StoryObj<AuthButtonType>;
+
+export default {
+  title: 'Components/AuthButton',
+  component: AuthButton,
+} as Meta<AuthButtonType>;
+
+const Template: StoryType = {
+  render: (args) => <AuthButton {...args} />,
+};
+
+export const Google: StoryType = { ...Template };
+
+export const Facebook: StoryType = { ...Template, args: { method: 'Facebook' } };
