36b5746f8a1ca1439deba313239ac5d788e54dd4
Luiz Meraki <luizollvrsantos@gmail.com>
2023-07-04 20:34:05 -0300

    feat: added value verification to reducer

--- src/utils/reducer.ts
+++ src/utils/reducer.ts
@@ -2,7 +2,7 @@ import { ReducerActionType, ReducerStateType } from '~/types/Form';
 
 export function reducer(state: ReducerStateType, action: ReducerActionType) {
   const property = action.property;
-  const value = action.payload.value;
+  const value = action.payload.value ?? state[property]!.value;
   const error = action.payload.error;
 
   return { ...state, [property]: { value, error } };
