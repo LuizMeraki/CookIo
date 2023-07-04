9a41230a8ef319f1c1a89ad4ac18234390ac03db
Luiz Meraki <luizollvrsantos@gmail.com>
2023-07-04 19:31:10 -0300

    feat: created ReducerStateType

--- src/types/Form.ts
+++ src/types/Form.ts
@@ -1,11 +1,23 @@
 import { Dispatch } from 'react';
 
+type StateValueType = {
+  value: string;
+  error: string | null;
+};
+
 export type PropertyType =
   | 'recipeName'
   | 'username'
   | 'email'
   | 'password'
   | 'confirmPassword';
+
+export type ReducerStateType = {
+  username?: StateValueType;
+  email?: StateValueType;
+  password?: StateValueType;
+  confirmPassword?: StateValueType;
+};
 
 export type ReducerActionType = {
   property: PropertyType;
