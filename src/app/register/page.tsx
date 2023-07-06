676ddc970709e062d093b0c7022145ac8f56b04b
Luiz Meraki <luizollvrsantos@gmail.com>
2023-07-06 13:21:14 -0300

    feat: added auth hook to register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,8 +5,10 @@ import { useReducer } from 'react';
 import Input from '~/components/Form/Input';
 import Button from '~/components/Form/Button';
 import withoutAuth from '~/components/HOC/withoutAuth';
+import PageLoading from '~/components/PageLoading';
 import RedirectLink from '~/components/Form/RedirectLink';
 
+import { useAuth } from '~/hooks/useAuth';
 import { useClientSide } from '~/hooks/useClientSide';
 
 import { initGoogleAuth } from '~/services/googleAuth';
@@ -19,6 +21,8 @@ import {
   handlePassword,
 } from '~/utils/handleChange';
 
+import { handleSubmit } from './helpers/handleSubmit';
+
 import styles from './styles.module.scss';
 
 const initialValue = {
@@ -30,76 +34,88 @@ const initialValue = {
 
 function Register() {
   const [state, dispatch] = useReducer(reducer, initialValue);
+  const [auth, isLoading] = useAuth();
 
   useClientSide(initGoogleAuth);
 
   return (
-    <main className={styles.main}>
-      <section className={styles.container}>
-        <h1>Junte-se a Nós e Faça Parte Agora da Comunidade de Receitas</h1>
+    <>
+      {isLoading && <PageLoading />}
 
-        <form className={styles.form}>
-          <div className={styles.inputGroup}>
-            <Input
-              label="Nome"
-              type="text"
-              placeholder="seu nome"
-              state={state.username!.value}
-              error={state.username!.error}
-              onChange={(e) => handleName(e, dispatch)}
-            />
-          </div>
+      <main className={styles.main}>
+        <section>
+          <h1>Junte-se a Nós e Faça Parte Agora da Comunidade de Receitas</h1>
 
-          <div className={styles.inputGroup}>
-            <Input
-              label="E-mail"
-              type="email"
-              placeholder="exemplo@gmail.com"
-              state={state.email!.value}
-              error={state.email!.error}
-              onChange={(e) => handleEmail(e, dispatch)}
-            />
-          </div>
+          <form
+            className={styles.form}
+            onSubmit={(e) => handleSubmit(e, state, auth, dispatch)}
+          >
+            <div className={styles.inputGroup}>
+              <Input
+                label="Nome"
+                type="text"
+                placeholder="seu nome"
+                state={state.username!.value}
+                error={state.username!.error}
+                onChange={(e) => handleName(e, dispatch)}
+              />
+            </div>
 
-          <div className={styles.inputGroup}>
+            <div className={styles.inputGroup}>
+              <Input
+                label="E-mail"
+                type="email"
+                placeholder="exemplo@gmail.com"
+                state={state.email!.value}
+                error={state.email!.error}
+                onChange={(e) => handleEmail(e, dispatch)}
+              />
+            </div>
+
+            <div className={styles.inputGroup}>
+              <Input
+                label="Criar Senha"
+                type="password"
+                placeholder="########"
+                state={state.password!.value}
+                error={state.password!.error}
+                onChange={(e) => handlePassword(e, state.confirmPassword!, dispatch)}
+              />
+            </div>
+
             <Input
-              label="Criar Senha"
+              label="Confirmar Senha"
               type="password"
               placeholder="########"
-              state={state.password!.value}
-              error={state.password!.error}
-              onChange={(e) => handlePassword(e, state.confirmPassword!, dispatch)}
+              state={state.confirmPassword!.value}
+              error={state.confirmPassword!.error}
+              onChange={(e) => handleConfirmPassword(e, state.password!.value, dispatch)}
             />
-          </div>
 
-          <Input
-            label="Confirmar Senha"
-            type="password"
-            placeholder="########"
-            state={state.confirmPassword!.value}
-            error={state.confirmPassword!.error}
-            onChange={(e) => handleConfirmPassword(e, state.password!.value, dispatch)}
-          />
+            <div className={styles.redirectLinkContainer}>
+              <RedirectLink href="/login">Já tem uma conta? Entre agora</RedirectLink>
+            </div>
 
-          <div className={styles.redirectLinkContainer}>
-            <RedirectLink href="/login">Já tem uma conta? Entre agora</RedirectLink>
-          </div>
+            <Button
+              type="submit"
+              title="cadastrar conta"
+              aria-label="cadastrar em cookio"
+            >
+              Cadastrar
+            </Button>
 
-          <Button type="submit" title="cadastrar conta" aria-label="cadastrar em cookio">
-            Cadastrar
-          </Button>
+            <span className={styles.textOr}>OU</span>
 
-          <span className={styles.textOr}>OU</span>
-
-          <div className={styles.otherMethodsContainer}>
-            <div
-              id="google-sign-in"
-              aria-label="fazer cadastro usando sua conta google"
-            />
-          </div>
-        </form>
-      </section>
-    </main>
+            <div className={styles.otherMethodsContainer}>
+              <div
+                id="google-sign-in"
+                aria-label="fazer cadastro usando sua conta google"
+              />
+            </div>
+          </form>
+        </section>
+      </main>
+    </>
   );
 }
 
