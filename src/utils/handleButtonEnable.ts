e00d39575d1b1af5d6f8a7caca039f0fc6fffa47
Luiz Meraki <luizollvrsantos@gmail.com>
2023-07-07 12:26:38 -0300

    refactor: renamed inputValue to input

--- src/utils/handleButtonEnable.ts
+++ src/utils/handleButtonEnable.ts
@@ -3,9 +3,7 @@ import { ReducerStateType } from '~/types/Form';
 export function handleButtonEnable(state: ReducerStateType, isLoading: boolean) {
   const inputValues = Object.values(state);
 
-  const isThereError = inputValues.some(
-    (inputValue) => typeof inputValue.error === 'string'
-  );
+  const isThereError = inputValues.some((input) => typeof input.error === 'string');
   const isInputFilled = !inputValues.every((input) => input.value.length > 0);
 
   return isThereError || isLoading || isInputFilled;
